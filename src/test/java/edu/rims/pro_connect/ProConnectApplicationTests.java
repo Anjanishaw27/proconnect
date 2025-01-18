e4f60228e8fc8e0818e6ba5770e35a0e2777509a
anjani <anjanishaw2003@gmail.com>
2025-01-18 12:09:28 +0530

    initial env setup

---
+++ src/test/java/edu/rims/pro_connect/ProConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rims.pro_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
