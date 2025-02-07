cd8f289e4de93e68d437d86a31df2a5bb9d923ac
anjani <anjanishaw2003@gmail.com>
2025-02-07 14:03:07 +0530

    hirepage

--- src/main/java/edu/rims/pro_connect/controller/HirenowController.java
+++ src/main/java/edu/rims/pro_connect/controller/HirenowController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HirenowController {
   @GetMapping("/hirenow")
   String hirenow() {
-    return "hirenow";
+    return "client/hirenow";
   }
 
 }
