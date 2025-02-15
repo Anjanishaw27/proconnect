f416ca013097445ca6f5c64d8d6d007c95fc770f
anjani <anjanishaw2003@gmail.com>
2025-02-15 10:07:53 +0530

    new changes

--- src/main/java/edu/rims/pro_connect/controller/AdminController.java
+++ src/main/java/edu/rims/pro_connect/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
   @GetMapping("/admindashboard")
  String admindashboard() {
-    return "/client/admindashboard";
+    return "/admindashboard";
 
   }
 
