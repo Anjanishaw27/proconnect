2eb46b6ada1fbab784cec8e36632b99eb9cf123c
Anjanishaw27 <anjanishaw2003@gmail.com>
2025-02-18 11:14:06 -0800

    added developer page

--- src/main/java/edu/rims/pro_connect/controller/ProfileController.java
+++ src/main/java/edu/rims/pro_connect/controller/ProfileController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/developer")
-public class DevloperController {
-    @GetMapping("/developer")
-    public String developer() {
-        return "client/developer";
-    } 
+@RequestMapping("/profile")
+public class ProfileController {
+    @GetMapping("/profile")
+    public String profile() {
+        return "client/profile";
+    }
+    
 }
