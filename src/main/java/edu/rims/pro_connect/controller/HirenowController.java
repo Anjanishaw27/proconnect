f085dda9c4bf56b5fc2b86a36062cd1d68b9dc4d
anjani <anjanishaw2003@gmail.com>
2025-03-07 11:10:18 +0530

    all new changes

--- src/main/java/edu/rims/pro_connect/controller/HirenowController.java
+++ src/main/java/edu/rims/pro_connect/controller/HirenowController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/hirenow")
 public class HirenowController {
   @GetMapping("/hirenow")
   String hirenow() {
