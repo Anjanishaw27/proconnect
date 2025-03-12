354a8aac1b962a9a1835f024d83cab01d8f3f7e1
Anjanishaw27 <anjanishaw2003@gmail.com>
2025-03-12 01:31:07 -0700

    my project html add

--- src/main/java/edu/rims/pro_connect/controller/ClientdashboardController.java
+++ src/main/java/edu/rims/pro_connect/controller/ClientdashboardController.java
@@ -1,12 +1,20 @@
 package edu.rims.pro_connect.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.rims.pro_connect.entity.User;
+import edu.rims.pro_connect.repository.ProjectRepository;
+import edu.rims.pro_connect.repository.UserRepository;
 
 @Controller
 @RequestMapping("/client")
 public class ClientdashboardController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/clientdashboard")
     String clientdashboard(){
         return "client/clientdashboard";
@@ -17,7 +25,9 @@ public class ClientdashboardController {
     }
     
     @GetMapping("/myproject")
-    String clientmyproject(){
+    String clientmyproject(Model model){
+        User user = userRepository.findById(1).orElseThrow();
+        model.addAttribute("user", user);
         return "client/myproject";
     }
 }
