c1df3d5a7e66d832667b71d0293b0241ad372663
Soumendra Kumar <soumendrabehera99@gmail.com>
2025-03-12 19:50:41 +0530

    project pdp and project list by status get and display

--- src/main/java/edu/rims/pro_connect/repository/ProjectRepository.java
+++ src/main/java/edu/rims/pro_connect/repository/ProjectRepository.java
@@ -1,10 +1,12 @@
 package edu.rims.pro_connect.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.rims.pro_connect.entity.Project;
 
-public interface ProjectRepository extends JpaRepository<Project, String>{
+public interface ProjectRepository extends JpaRepository<Project, String> {
 
-    
+    List<Project> findByCategoryCategoryIdAndProjectStatus(String categoryId, String projectStatus);
 }
